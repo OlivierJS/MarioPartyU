0dc1fb2d44a2617c37305f234604d40489274b88
OlivierJS <79011256+OlivierJS@users.noreply.github.com>
2021-03-23 22:58:22 +0100

    Platforming Game implemented
    Druk op V om de minigame te testen vanuit de gameboard

---
+++ Assets/Scripts/Minigames/Bullet.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public void Setup(int dir, float spd)
+    {
+        direction = dir;
+        speed = spd;
+    }
+    
+    public float speed = 15.0f;
+    Rigidbody rb;
+    public Vector3 movement;
+    public int direction = 0; //0 = right, 1 = left, 2 = up, 3 = down
+    PlatformingManager thePlatformingManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlatformingManager = GameObject.FindObjectOfType<PlatformingManager>();
+        gameObject.tag = "Bullet";
+        rb = this.GetComponent<Rigidbody>();
+        switch (direction)
+        {
+            case 0:
+                movement = new Vector3(1f, 0f, 0f);
+                break;
+            case 1:
+                movement = new Vector3(-1f, 0f, 0f);
+                break;
+            case 2:
+                movement = new Vector3(0, 0f, 1f);
+                break;
+            case 3:
+                movement = new Vector3(0f, 0f, -1f);
+                break;
+        }
+        StartCoroutine(destroySelf());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (thePlatformingManager.timeUp == true || thePlatformingManager.P1Win == true || thePlatformingManager.P2Win == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator destroySelf()
+    {
+        yield return new WaitForSeconds(10f);
+        Destroy(gameObject);
+    }
+
+    void FixedUpdate()
+    {
+        moveBullet(movement);
+    }
+
+    void moveBullet(Vector3 direction)
+    {
+        rb.MovePosition((Vector3)transform.position + (direction * speed * Time.deltaTime));
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Minigames/BulletLauncher.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLauncher : MonoBehaviour
+{
+    [SerializeField] private Transform Bullet;
+    public int direction = 0; //0 = right, 1 = left, 2 = up, 3 = down
+    Quaternion rotation;
+    public float interval;
+    PlatformingManager thePlatformingManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlatformingManager = GameObject.FindObjectOfType<PlatformingManager>();
+        interval = Random.Range(3.0f, 20.0f);
+        switch (direction)
+        {
+            case 0:
+                rotation = Quaternion.Euler(0, 0, 90);
+                break;
+            case 1:
+                rotation = Quaternion.Euler(0, 0, 90);
+                break;
+            case 2:
+                rotation = Quaternion.Euler(90, 0, 0);
+                break;
+            case 3:
+                rotation = Quaternion.Euler(90, 0, 0);
+                break;
+        }
+        StartCoroutine(randomFire(interval));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void shootBullet()
+    {
+        Transform bulletTransform = Instantiate(Bullet, transform.position, rotation);
+
+        int dir = direction;
+        float spd = Random.Range(5.0f, 12.0f);
+        bulletTransform.GetComponent<Bullet>().Setup(dir, spd);
+    }
+
+    IEnumerator randomFire(float interval)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(interval);
+            if (thePlatformingManager.timeUp != true && thePlatformingManager.P1Win != true && thePlatformingManager.P2Win != true)
+            {
+                shootBullet();
+            }
+        }
+    }
+}
